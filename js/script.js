fcaf3d92b9ecbf75f7710ebcb2583aacad8c664c
francescapetra <francescalaurapetra@gmail.com>
2021-03-05 17:15:59 +0100

    input + calcolo costo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
+var calculator = document.getElementById('calculate');
 
+var name_hamb =  document.getElementById('name_hamburgher');
+
+var ingredients = document.getElementsByName('prodotto')
+ingredients.className = "more_ingredients";
+
+var carnet = ["promo20", "ciao20", "benvenuto20"];
+
+var coupon = document.getElementById('calculate');
+
+var aggiunta = 0;
+
+var somma = 50;
+
+
+calculator.addEventListener("click",
+  function(){
+    if (name_hamb.value != "" ) {
+    }else {
+      console.log(alert("Inserisci il nome del panino"));
+    }
+    for (var i = 0; i < ingredients.length; i++) {
+      if (ingredients[i].checked) {
+        aggiunta += parseInt(ingredients[i].value);
+        somma = somma + aggiunta;
+        }
+      }
+        // if (carnet.includes(coupon.value)) {
+        //   somma -= somma * 0,2;
+        // }
+
+
+
+      document.getElementById('big_total').innerHTML =  somma + "$";
+    }
+);
